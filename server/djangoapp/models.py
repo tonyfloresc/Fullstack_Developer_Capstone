02746274b1bb614d3e428f6ad17f2f920997f997
tonyfloresc <luis.floresco@udlap.mx>
2025-09-11 14:44:20 -0400

    Django: add CarMake/CarModel, admin registration, get_cars endpoint, populate data

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,51 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+# ---------- CarMake ----------
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # (Opcional) otros campos de tu preferencia, por ejemplo:
+    # country = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        # Representación legible del objeto
+        return self.name
 
 
-# Create your models here.
+# ---------- CarModel ----------
+class CarModel(models.Model):
+    # Relación muchos-a-uno con CarMake (un make puede tener muchos modelos)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    # Dealer Id (entero) que referencia al dealer en la base de datos externa
+    dealer_id = models.IntegerField()
 
+    name = models.CharField(max_length=100)
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV',   'SUV'),
+        ('WAGON', 'Wagon'),
+        # Puedes añadir más si quieres:
+        # ('COUPE', 'Coupe'), ('TRUCK', 'Truck'),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+
+    # Año (usa el rango sugerido por la muestra)
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+
+    # (Opcional) otros campos
+    # trim = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        # Imprime "Make Model" para que sea claro
+        return f"{self.car_make} {self.name}"
+
--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,52 @@
+from .models import CarMake, CarModel
+
 def initiate():
-    print("Populate not implemented. Add data manually")
+    car_make_data = [
+        {"name": "NISSAN",   "description": "Great cars. Japanese technology"},
+        {"name": "Mercedes", "description": "Great cars. German technology"},
+        {"name": "Audi",     "description": "Great cars. German technology"},
+        {"name": "Kia",      "description": "Great cars. Korean technology"},
+        {"name": "Toyota",   "description": "Great cars. Japanese technology"},
+    ]
+
+    # Crear marcas
+    car_make_instances = []
+    for data in car_make_data:
+        car_make_instances.append(
+            CarMake.objects.create(
+                name=data["name"],
+                description=data["description"]
+            )
+        )
+
+    # Crear modelos (con dealer_id)
+    car_model_data = [
+        {"name": "Pathfinder", "type": "SUV",   "year": 2023, "car_make": car_make_instances[0], "dealer_id": 101},
+        {"name": "Qashqai",    "type": "SUV",   "year": 2023, "car_make": car_make_instances[0], "dealer_id": 102},
+        {"name": "XTRAIL",     "type": "SUV",   "year": 2023, "car_make": car_make_instances[0], "dealer_id": 103},
+
+        {"name": "A-Class",    "type": "SUV",   "year": 2023, "car_make": car_make_instances[1], "dealer_id": 104},
+        {"name": "C-Class",    "type": "SUV",   "year": 2023, "car_make": car_make_instances[1], "dealer_id": 105},
+        {"name": "E-Class",    "type": "SUV",   "year": 2023, "car_make": car_make_instances[1], "dealer_id": 106},
+
+        {"name": "A4",         "type": "SUV",   "year": 2023, "car_make": car_make_instances[2], "dealer_id": 107},
+        {"name": "A5",         "type": "SUV",   "year": 2023, "car_make": car_make_instances[2], "dealer_id": 108},
+        {"name": "A6",         "type": "SUV",   "year": 2023, "car_make": car_make_instances[2], "dealer_id": 109},
+
+        {"name": "Sorrento",   "type": "SUV",   "year": 2023, "car_make": car_make_instances[3], "dealer_id": 110},
+        {"name": "Carnival",   "type": "SUV",   "year": 2023, "car_make": car_make_instances[3], "dealer_id": 111},
+        {"name": "Cerato",     "type": "Sedan", "year": 2023, "car_make": car_make_instances[3], "dealer_id": 112},
+
+        {"name": "Corolla",    "type": "Sedan", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 113},
+        {"name": "Camry",      "type": "Sedan", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 114},
+        {"name": "Kluger",     "type": "SUV",   "year": 2023, "car_make": car_make_instances[4], "dealer_id": 115},
+    ]
+
+    for data in car_model_data:
+        CarModel.objects.create(
+            name=data["name"],
+            car_make=data["car_make"],
+            type=data["type"],
+            year=data["year"],
+            dealer_id=data["dealer_id"]
+        )
